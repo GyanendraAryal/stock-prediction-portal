948d5a808b9ae72e98b90c976d1548bdfd22885e
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-09-12 21:32:20 +0545

    frontend and backend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Hello from "./components/Hello";
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-orange-500 text-center">
+        Stock Prediction Web App
+      </h1>
+      <Hello />
+    </>
+  );
+}
+
+export default App;
