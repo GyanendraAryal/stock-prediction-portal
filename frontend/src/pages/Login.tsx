8727d053a3d5c455afc9d63ae3e8090d6c27033c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-09-15 11:36:02 +0545

    setting up JWT based authentication and authorization

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { Link } from "react-router";
+import axios from "axios";
 
 function Login() {
   type LoginFormData = {
@@ -14,8 +15,16 @@ function Login() {
     formState: { errors },
   } = useForm<LoginFormData>();
 
-  const onSubmit: SubmitHandler<LoginFormData> = (data) => {
+  const onSubmit: SubmitHandler<LoginFormData> = async (data) => {
     console.log(data);
+    try {
+        const response = await axios.post("http://localhost:8000/api/v1/token/", data);
+        console.log("Response",response.data);
+
+    } catch (error) {
+        console.log(error);
+
+    }
   };
 
   return (
