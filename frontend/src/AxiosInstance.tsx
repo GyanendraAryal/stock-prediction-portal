e0004ae86291c5e04aa2e154b777dec262e8c41d
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-09-16 13:22:54 +0545

    response interceptor and authorization management

--- frontend/src/AxiosInstance.tsx
+++ frontend/src/AxiosInstance.tsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 const baseURL = import.meta.env.VITE_BACKEND_BASE_API;
 const AxiosInstance = axios.create({
   baseURL: baseURL,
@@ -16,10 +15,40 @@ AxiosInstance.interceptors.request.use(
     if (accessToken) {
       config.headers["Authorization"] = `Bearer ${accessToken}`;
     }
-    console.log("After auth:", config);
+    // console.log("After auth:", config);
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// Response interceptor
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  // Handling failed requests
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest.retry) {
+      originalRequest.retry = true;
+      const refreshToken = localStorage.getItem("refreshToken");
+      try {
+        const response = await AxiosInstance.post("/token/refresh/", {
+          refresh: refreshToken,
+        });
+        console.log("New Response: ",response.data.access);
+        localStorage.setItem("accessToken", response.data.access);
+        originalRequest.headers["Authorization"] =
+          `Bearer ${response.data.access}`;
+        return AxiosInstance(originalRequest);
+      } catch (error) {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("refreshToken");
+        window.location.href = "/login";
+      }
+    }
     return Promise.reject(error);
   },
 );
